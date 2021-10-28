414869dbb80515c27f6c362af405b38dccab4944
williansamer <willian.samer@gmail.com>
2021-10-28 15:57:32 -0300

    created the delete button

--- src/Todo.js
+++ src/Todo.js
@@ -6,19 +6,25 @@ import Item from "./components/Items";
 
 function Todo(){
 
-    const [item, setItem] = useState([]); //array of objects with the values of the input.
+    const [itens, setItens] = useState([]); //array of objects with the values of the input.
 
     function onAddItem(text){ //this function is called from the TodoForm component with the text value.
         let newItem = new Item(text); //creates a new Item object with the text value.
 
-        setItem([...item, newItem]); //the '...item' is used to spread the array and add the newItem value.
+        setItens([...itens, newItem]); //the '...item' is used to spread the array and add the newItem value.
+    }
+
+    function onDeleteItem(item){
+        let delItem = itens.filter((it) => it.id !== item.id); //filters the array and returns the item that is not the same as the item that is being deleted.
+    
+        setItens(delItem); //sets the new array.
     }
 
     return(
         <div className="container">
         <h1>Todo</h1>
-        <TodoForm onAddItem={onAddItem}/>
-        <List item={item}/>
+        <TodoForm onAddItem={onAddItem}/> {/* '{onAddItem}' is passed to the List component as a prop. From son to parent. */}
+        <List onDeleteItem={onDeleteItem} itens={itens}/> {/* '{onDeleteItem}' is passed to the List component as a prop. From son to parent. */}
         </div>
     )
 }
--- src/components/List.js
+++ src/components/List.js
@@ -3,8 +3,10 @@ import React from "react";
 function List(props){
     return(
         <ul>
-            {props.item.map((item)=>{return <li key={item.id}>{item.text}</li>})}
-        </ul>
+            {props.itens.map((item)=>{
+                return <li key={item.id}>{item.text}
+                        <button onClick={()=>props.onDeleteItem(item)}><img alt="delete" src="../../assets/bin.png"></img></button></li>})}
+        </ul> //'{()=>props.onDeleteItem(item)}' is an anonymous function, which takes an item as a parameter and calls the onDeleteItem function passing the item as a parameter to the parent component.
     )
 }
 
