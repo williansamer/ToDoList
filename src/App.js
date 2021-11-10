f538a507bb30730898638bf00194af6edf15beab
williansamer <willian.samer@gmail.com>
2021-11-10 13:01:55 -0300

    Saving and Loading state from localStorage

--- src/App.js
+++ src/App.js
@@ -1,57 +1,32 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import "./App.css";
 import List from "./components/List";
 import TodoForm from "./components/TodoForm";
 import Modal from "./components/Modal";
-import Item from "./components/Items";
 import { createStore } from "redux";
 import reducerItem from "./reducers/reducerItem";
 import { Provider } from "react-redux";
 
 const SAVED_ITEMS = "savedItem";
 
-const store = createStore(reducerItem);
+function persistState(state){ //save the state to localStorage when the state changes
+    localStorage.setItem(SAVED_ITEMS, JSON.stringify(state));
+}
+function loadState(){ //load the state from localStorage
+    const actualState = localStorage.getItem(SAVED_ITEMS);
+    if(actualState){
+        return JSON.parse(actualState);
+    } else{
+        return [];
+    }
+}
+
+const store = createStore(reducerItem, loadState()); // Creating a store with the reducerItem function and passing the current state of the store(loadState)
+
+store.subscribe(()=>{persistState(store.getState())}); // Saving the state of the store in localStorage always when the state changes.
 
 function App(){
-
     const [showModal, setShowModal] = useState(false);
-    //const [itens, setItens] = useState([]); //array of objects with the values of the input.
-    
-/* 
-    useEffect(() =>{ //this useEffect is a hook that runs after the component is rendered because there is no dependency.
-        const savedItens = JSON.parse(localStorage.getItem(SAVED_ITEMS));
-        if(savedItens){
-            setItens(savedItens);
-        }
-    }, []);
-
-    useEffect(()=>{ //this useEffect is a hook that runs after the state is updated, because there is a dependency(itens).
-        localStorage.setItem(SAVED_ITEMS, JSON.stringify(itens));
-    }, [itens]);
-
-    function onAddItem(text){ //this function is called from the TodoForm component with the text value.
-        let newItem = new Item(text); //creates a new Item object with the text value.
-
-        setItens([...itens, newItem]); //the '...item' is used to spread the array and add the newItem value.
-        onHideModal();
-    }
-
-    function onDeleteItem(item){
-        let delItem = itens.filter((it) => it.id !== item.id); //filters the array and returns the item that is not the same as the item that is being deleted.
-    
-        setItens(delItem); //sets the new array.
-    }
-
-    function onDone(check){
-        let doneItem = itens.map((it)=>{
-            if(it.id === check.id){
-                it.done = !it.done; //changes the done value of the item.
-            }
-            return it; //returns the item with the new done value to the array.
-        })
-
-        setItens(doneItem); //sets the new array.
-    } */
 
     function onHideModal(){
         setShowModal(false);
